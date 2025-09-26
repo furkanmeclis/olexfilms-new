54cfde9252b8e9fbecb4a19b8497d02fdaff4ce7
Furkan Meclis <furkanmeclis@icloud.com>
2025-09-26 17:26:20 +0300

    .gitignore dosyasına /public/uploads klasörü eklendi. filesystems.php dosyasında varsayılan dosya sistemi 'public' olarak güncellendi ve dosya yolu doğrudan public/uploads klasörüne yönlendirildi.

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'public'),
 
     /*
     |--------------------------------------------------------------------------
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -74,7 +74,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        // Sembolik link kullanılmıyor, dosyalar doğrudan public/uploads klasörüne kaydediliyor
     ],
 
 ];
